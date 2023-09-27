ad90aa0dc6df96ad60a751d0b3d1622678c5d667
Vega <2187815@cognizant.com>
2023-09-27 09:00:57 -0300

    Certs section added.

---
+++ Portfolio/Models/Certificate.cs
@@ -0,0 +1,11 @@
+﻿namespace Portfolio.Models
+{
+    public class Certificate
+    {
+        public string Image { get; set; }
+        public string Link { get; set; }
+        public string Title { get; set; }
+        public string Info { get; set; }
+        public string HighlightedText { get; set; }
+    }
+}
