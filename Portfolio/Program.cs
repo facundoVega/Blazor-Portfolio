0d73f4da278e08f2b508036eac9b8e0c1368ad94
Vega <2187815@cognizant.com>
2023-09-29 09:03:57 -0300

    Skill carousel completed.

--- Portfolio/Program.cs
+++ Portfolio/Program.cs
@@ -1,3 +1,4 @@
+using BlazorStrap;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Portfolio;
@@ -5,6 +6,6 @@ using Portfolio;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
+builder.Services.AddBlazorStrap();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 await builder.Build().RunAsync();
